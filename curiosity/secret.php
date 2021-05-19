4610919b2f820c0e6dc86587a7b03341374b4dc3
open seven six ate <open768@yahoo.co.uk>
2021-05-19 21:54:43 +0100

    app sets the facebookk app id - no longer global

---
+++ curiosity/secret.php
@@ -0,0 +1,6 @@
+<?php
+	cSecret::$FB_APP = "130383662364349";
+	cSecret::$FB_SECRET = "e18366b18abfead1fc1e5f57495c0c8d";
+	cSecret::$FB_DEV_APP = "130383662364349";
+	cSecret::$FB_DEV_SECRET = "e18366b18abfead1fc1e5f57495c0c8d";
+?>
