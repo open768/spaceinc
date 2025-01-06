05186eb61bd92e1a103dbff630f99129706c89e0
sunil <cluck@chickenkatsu.co.uk>
2025-01-06 00:43:10 +0000

    another small step for  #7

--- db/mission-manifest.php
+++ db/mission-manifest.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Database\Capsule\Manager as CapsuleManager;
 
+
 class cMissionManifest {
-    static $CapsuleManager = null;
+    const DBNAME = "manifest_orm.db";
+
+    static $capsuleManager = null;
 
     static function init() {
         //check that database class has been loaded - redundant as composer would throw an error if it wasnt
@@ -12,19 +15,22 @@ class cMissionManifest {
             cDebug::error("check composer - unable to find class $classname");
         cDebug::extra_debug("found class $classname");
 
+        //create sqlite database if it does not exist
+        $oDB = new cSqlLite(self::DBNAME);
+        $sDBPath = $oDB->path;
+
         //connect the ORM to a SQL lite database
-        if (self::$CapsuleManager == null) {
+        if (self::$capsuleManager == null) {
 
             $oManager = new CapsuleManager();
-            $sDBFile = cAppGlobals::$dbRoot . '/maniorm.db';
             $oManager->addConnection([
                 'driver' => 'sqlite',
-                'database' => $sDBFile,
+                'database' => $sDBPath,
             ]);
             $oManager->setAsGlobal();
             $oManager->bootEloquent();
-            self::$CapsuleManager = $oManager;
-            cDebug::extra_debug("booted eloquent - db is $sDBFile");
+            self::$capsuleManager = $oManager;
+            cDebug::extra_debug("started eloquent - db is $sDBPath");
         }
     }
 }
