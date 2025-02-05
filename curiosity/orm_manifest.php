fd4731f7fff6fe8e8bc4f9f491a0b630f0af794b
sunil <cluck@chickenkatsu.co.uk>
2025-02-05 22:39:17 +0000

    incremental commit

--- curiosity/orm_manifest.php
+++ curiosity/orm_manifest.php
@@ -1,6 +1,4 @@
 <?php
-require_once "manifest.php";
-require_once cAppGlobals::$spaceInc . "/db/mission-manifest.php";
 
 /**************************************************************************
 Copyright (C) Chicken Katsu 2013 -2024
@@ -12,8 +10,18 @@ http://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
 For licenses that allow for commercial use please contact cluck@chickenkatsu.co.uk
 // USE AT YOUR OWN RISK - NO GUARANTEES OR ANY FORM ARE EITHER EXPRESSED OR IMPLIED
  **************************************************************************/
+require_once "manifest.php";
+require_once cAppGlobals::$spaceInc . "/db/mission-manifest.php";
+require_once  cAppGlobals::$spaceInc . "/curiosity/curiosity.php";
+
 
 class cCuriosityORMManifest {
+    static $mission_id = null;
+
+    static function init() {
+        $mission_id = tblMissions::get_id(null, cCuriosity::MISSION_ID);
+    }
+
     static function empty_ORM_tables() {
         //drop all tables in the manifest
         cDebug::enter();
@@ -23,9 +31,91 @@ class cCuriosityORMManifest {
 
     static function updateIndex() {
         cDebug::enter();
-        //reset index status for DEBUGGING PRUPOSES
-        cCuriosityManifestIndexStatus::clear_status();
 
+        //reset index status for DEBUGGING PRUPOSES
+        //cCuriosityManifestIndexStatus::clear_status();
+
+        //----------get manifest
+        cDebug::write("getting sol Manifest");
+        $oManifest = cCuriosityManifest::getManifest();
+
+        //----------get status from odb
+        $sStatus = cCuriosityManifestIndexStatus::get_status();
+
+        if ($sStatus === cCuriosityManifestIndexStatus::STATUS_COMPLETE) {
+            $sLastIndexedSol = cCuriosityManifestIndexStatus::get_last_indexed_sol();
+            $sLatestManifestSol = $oManifest->latest_sol;
+            cDebug::write("last indexed sol was: $sLastIndexedSol, latest manifest sol: $sLatestManifestSol");
+            if ($sLastIndexedSol >= $sLatestManifestSol)
+                cDebug::error("indexing allready complete");
+        }
+
+        //----------get last indexed sol  odb
+        $sLastSol = cCuriosityManifestIndexStatus::get_last_indexed_sol();
+        if ($sLastSol == null) $sLastSol = -1;
+        cDebug::write("indexing starting at sol: $sLastSol");
+
+        cDebug::write("processing sol Manifest");
+        $aSols = $oManifest->sols;
+        ksort($aSols, SORT_NUMERIC);
+
+
+        //---------------iterate manifest
+        foreach ($aSols as $number => $oSol) {
+            $sSol = $oSol->sol;
+            $bReindex = false;
+
+            //- - - - - - - - -check when SOL was  last updated
+            $sStoredLastUpdated = cCuriosityManifestIndexStatus::get_sol_last_updated($sSol);
+            $sManifestLastUpdated = $oSol->last_updated;
+            //cDebug::write("stored lastindex:$sStoredLastUpdated, manifest date:$sManifestLastUpdated");
+            if ($sStoredLastUpdated == null)
+                $bReindex = true;
+            elseif ($sStoredLastUpdated < $sManifestLastUpdated)
+                $bReindex = true;
+
+            //---------------------check if the row needs reindexing
+            if (!$bReindex) $bReindex = $sSol > $sLastSol;
+            if (!$bReindex) continue;
+
+            //-------perform the index
+            TransactionsORM::beginTransaction();
+            try {
+                self::index_sol($sSol, $sManifestLastUpdated, $bReindex);
+                TransactionsORM::commit();
+            } catch (Exception $e) {
+                TransactionsORM::rollBack();
+                cDebug::error("unable to index sol $sSol: $e ");
+            }
+        }
+        cCuriosityManifestIndexStatus::put_status(cCuriosityManifestIndexStatus::STATUS_COMPLETE);
         cDebug::leave();
     }
+
+    //*****************************************************************************
+    static function index_sol(string $psSol, $sLastUpdatedValue, bool $pbReindex) {
+        cDebug::extra_debug("indexing sol:$psSol");
+
+        if ($pbReindex) self::delete_sol_index($psSol);
+
+        $bCheckExpiry = !$pbReindex;
+        $oSolData = cCuriosityManifest::getSolRawData($psSol, $bCheckExpiry); //this is needed
+
+        $aImages = $oSolData->images;
+        if ($aImages === null) cDebug::error("no image data");
+
+        foreach ($aImages as $sKey => $oImgData)
+            self::add_to_index($psSol, $oImgData);
+
+
+        if (cDebug::is_debugging())
+            cPageOutput::scroll_to_bottom();
+
+
+        //update the status
+        cCuriosityManifestIndexStatus::put_last_indexed_sol($psSol);
+        cCuriosityManifestIndexStatus::put_sol_last_updated($psSol, $sLastUpdatedValue);
+    }
 }
+
+cCuriosityORMManifest::init();
--- db/mission-manifest.php
+++ db/mission-manifest.php
@@ -1,8 +1,6 @@
 <?php
 
 //ORM is eloquent: https://github.com/illuminate/database
-use Illuminate\Database\Capsule\Manager as CapsuleManager;
-use Illuminate\Database\Console\TableCommand;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Schema\Blueprint;
 
@@ -145,41 +143,13 @@ class cMissionManifest {
         tblMissions::truncate();
     }
 
-
-    //**********************************************************************************************
     static function init() {
-        //check that database class has been loaded - redundant as composer would throw an error if it wasnt
-        $classname = "Illuminate\Database\Capsule\Manager";
-        if (!class_exists($classname))
-            cDebug::error("check composer - unable to find class $classname");
-        cDebug::extra_debug("found class $classname");
-
-        //check pdo extension is loaded
-        if (!extension_loaded("pdo_sqlite"))
-            cDebug::error("pdo_sqlite extension is not loaded");
-
-        //create sqlite database if it does not exist
-        $oDB = new cSqlLite(self::DBNAME);
-        $sDBPath = $oDB->path;
-        cDebug::write("DB path is $sDBPath");
-
-        //connect the ORM to a SQL lite database
         if (self::$capsuleManager == null) {
-
-            $oManager = new CapsuleManager();
-            $oManager->addConnection([
-                'driver' => 'sqlite',
-                'database' => $sDBPath,
-                'prefix' => ''
-            ]);
-            $oManager->setAsGlobal();       //should be optional but isnt
-            $oManager->bootEloquent();
-            self::$capsuleManager = $oManager;
-            cDebug::extra_debug("started eloquent - db is $sDBPath");
-
-            //create the table if it does not exist
-            self::check_tables();
+            $oCapsule = cEloquentORM::init_db(self::DBNAME);
+            self::$capsuleManager = $oCapsule;
         }
+        //create the table if it does not exist
+        self::check_tables();
     }
 }
 cMissionManifest::init();
